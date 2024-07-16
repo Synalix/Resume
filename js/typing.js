76c51f36fd850dd842e60762dcc66248accd9d03
Synalix <95099886+Synalix@users.noreply.github.com>
2024-07-16 13:30:32 +0300

    update

--- js/typing.js
+++ js/typing.js
@@ -1,74 +1,29 @@
 function startTypingAnimation() {
-    var text = "Theodore Kouvelas";
+    var text = "Thodoris Kouvelas";
+    var text2 = "Email: thodoriskouvelas@gmail.com | Phone: 306907195744 | Discord: Theo_k.";
     var header = document.querySelector('header h1');
+    var header2 = document.querySelector('header p'); 
     var index = 0;
+    var index2 = 0;
 
     function type() {
         if (index < text.length) {
             header.innerHTML += text.charAt(index);
             index++;
-            setTimeout(type, 100); // Adjust typing speed here (100ms delay)
+            setTimeout(type, 60);
         }
     }
 
-    // Clear the header text and start the typing animation
-    header.innerHTML = '';
-    type();
+    function type2(){
+        if (index2 < text2.length) {
+            header2.innerHTML += text2.charAt(index2);
+            index2++;
+            setTimeout(type2, 25);
+
+    }
 }
-
-
-
-
-// Start the typing animation when the DOM is fully loaded
+    type();  type2();
+   
+}
 document.addEventListener('DOMContentLoaded', startTypingAnimation);
 
-
-
-
-function startTypingAnimation2() {
-    var text = "Email: thodoriskouvelas@gmail.com | Phone: 30+6907195744 | Discord: Theo_k.";
-    var header = document.querySelector('header p');
-    var index = 0;
-
-    function type() {
-        if (index < text.length) {
-            header.innerHTML += text.charAt(index);
-            index++;
-            setTimeout(type, 55); // Adjust typing speed here (100ms delay)
-        }
-    }
-
-    // Clear the header text and start the typing animation
-    header.innerHTML = '';
-    type();
-}
-
-// Function to start the typing animation after a 3-second delay
-function startTypingAnimationWithDelay2() {
-    setTimeout(startTypingAnimation2, 2000); // 3000ms = 3 seconds
-}
-
-// Start the typing animation when the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', startTypingAnimationWithDelay2);
-
-
-
-
-
-function startTypingAnimation3(text, target) {
-  //  var text = "Theodore Kouvelas";
-    var header = document.querySelector(target);
-    var index = 0;
-console.log(text.length);
-    function type() {
-        if (index < text.length) {
-            header.innerHTML += text.charAt(index);
-            index++;
-            setTimeout(type, 100); // Adjust typing speed here (100ms delay)
-        }
-    }
-
-    // Clear the header text and start the typing animation
-    header.innerHTML = '';
-    type();
-}
